8dccda0e71420e4e198fffea41d01e28c0c4debc
Arvind <arvind16007@gmail.com>
2018-04-09 02:12:27 +0530

    Added many more things to the chat app

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
+    var messages = [];
+    var activeUsers = [];
+
     console.log('New user connected ');
     socket.emit('newMessage', {
         'from': 'Arvind Kumar Sharma',
@@ -19,18 +22,33 @@ io.on('connection', (socket) => {
         'createdAt': Date.now().toLocaleString()
     });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('New Message created ', newMessage);
-        io.emit('newMessage', {
-            from: newMessage.from,
-            text: newMessage.text,
-            createdAt: newMessage.createdAt
-        });
-        console.log('After IO.EMIT');
+    socket.on('registerNewUser',function(userName){
+        console.log(userName + ' Registered for chatting.');
+        activeUsers.push(userName);
+    });
+    socket.broadcast.emit('joinWelcomeMessage');
+
+    socket.on('createMessage', (createdMessage) => {
+        console.log('New Message created ', createdMessage);
+        messages.push(createdMessage);
+        console.log(createdMessage);
+        //The following code will emit the action to all connected ports 
+        // //Including the person who sends something over the server
+        // io.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: newMessage.createdAt
+        // });
+
+        //The following piece of code enables us to broadcast the message 
+        //The sender of the message will not receive the sent message back from the server
+
+        socket.broadcast.emit('newMessage',createdMessage );      
     });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected.');
+        socket.broadcast.emit('userDisconnected');
     });
 });
 
